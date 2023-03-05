dde3e8370958e5fde35642713f19bb9a1454a433
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-03-05 04:45:17 -0500

    added jumping, gravity and game over

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
 
 #Display
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
@@ -23,39 +24,60 @@ snail_rect = snail_surface.get_rect(midbottom = (0, 300))
 #Player
 player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (100 , 300))
+player_gravity = 0
 
 #Game Loop
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:   #If player presses "X" button, game will close
             pygame.quit()
             exit()
-        #if event.type == pygame.MOUSEMOTION:
-        #    if player_rect.collidepoint(event.pos): print('mouse collides with player')
-    
-    #blits for background
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-    screen.blit(score_surface, score_rect)
+        if event.type == pygame.KEYDOWN:  #if player presses "ESC" button, game will close
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
 
-    #blits for snail
-    screen.blit(snail_surface, snail_rect)
-    snail_rect.left -= 4
-    if snail_rect.left < -80: snail_rect.left = 800  #respawn the snail after leaving the map
-    
-    #blits for player
-    screen.blit(player_surface, player_rect)
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:  #Jumping with Clicks
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:    #Jumping with SPACE mechanic
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: 
+                game_active = True
+                snail_rect.left = 800
 
 
-    #Rectangle collision
-    #if player_rect.colliderect(snail_rect):
-    #    print('collision')
-    
-    #mouse_pos = pygame.mouse.get_pos()
-    #if player_rect.collidepoint(mouse_pos):
-    #    print(pygame.mouse.get_pressed())
+    if game_active:
+        #blits for background
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,300))
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        screen.blit(score_surface, score_rect)
+
+        #blits for snail
+        screen.blit(snail_surface, snail_rect)
+        snail_rect.left -= 4
+        if snail_rect.left < -80: snail_rect.left = 800  #respawn the snail after leaving the map
+        
+        #blits for player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300 : player_rect.bottom = 300
+        screen.blit(player_surface, player_rect)
+
+        #collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('yellow')
+        
+
+
+
     
     
     pygame.display.update()
